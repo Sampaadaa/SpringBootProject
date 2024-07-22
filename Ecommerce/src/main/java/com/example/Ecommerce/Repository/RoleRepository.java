a492123987fde355cc2bbd072382e8099c3e1539
Sampaadaa <sampadha.kharel@gmail.com>
2024-07-22 11:58:02 +0545

    first SpringBoot project and Profile setup for dev env

--- Ecommerce/src/main/java/com/example/Ecommerce/Repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Ecommerce.Repository;
-
-import com.example.Ecommerce.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role , Long> {
-}
